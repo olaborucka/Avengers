03671dc9ce41ef833a67015f49744bc2c7d9549c
Aleksandra Borucka <a.borucka.278@studms.ug.edu.pl>
2026-03-17 22:03:11 +0100

    lab2

--- migrations/20260304112447_02_add_new_columns.js
+++ migrations/20260304112447_02_add_new_columns.js
@@ -3,17 +3,41 @@
  * @returns { Promise<void> }
  */
 exports.up = async function(knex) {
-    // 1. Dodajemy nową kolumnę do tabeli heroes
-    await knex.schema.alterTable('heroes', function(table) {
-        table.integer('missions_count').notNullable().defaultTo(0);
+    // 1. Tabela heroes
+    await knex.schema.createTable('heroes', function(table) {
+        table.increments('id');
+        table.string('name').unique().notNullable();
+        table.enu('power', ['flight', 'strength', 'telepathy', 'speed', 'invisibility']).notNullable();
+        
+        // Tutaj doklejone defaultTo('available')
+        table.enu('status', ['available', 'busy', 'retired']).notNullable().defaultTo('available');
+        
+        table.timestamps(true, true);
     });
 
-    // 2. Dodajemy nowe kolumny do tabeli incidents
-    await knex.schema.alterTable('incidents', function(table) {
-        table.string('district').nullable();
-        table.timestamp('assigned_at').nullable();
-        table.timestamp('resolved_at').nullable();
+    // 2. Tabela incidents
+    await knex.schema.createTable('incidents', function(table) {
+        table.increments('id');
+        table.string('location').notNullable();
+        table.enu('level', ['low', 'medium', 'critical']).notNullable();
+        
+        // Tutaj doklejone defaultTo('open')
+        table.enu('status', ['open', 'assigned', 'resolved']).notNullable().defaultTo('open');
+        
+        table.integer('hero_id')
+            .unsigned()
+            .references('id')
+            .inTable('heroes')
+            .onDelete('SET NULL');
+            
+        table.timestamps(true, true);
     });
+};
+
+exports.down = async function(knex) {
+    // Odwrotna kolejność usuwania! Najpierw incidents, potem heroes.
+    await knex.schema.dropTable('incidents');
+    await knex.schema.dropTable('heroes');
 };
 
 
